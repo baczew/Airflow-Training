b4059ed1aaa7f0fc75a4d2081c6916e9fb1d00d1
baczew <71928772+baczew@users.noreply.github.com>
2022-10-20 12:14:42 +0200

    1.0.1

--- Bitcoin_ETL.py
+++ Bitcoin_ETL.py
@@ -9,9 +9,14 @@ import os
 import sqlalchemy as sq
 import uuid
 
+default_args = {'start_date': datetime(year=2022, month=10, day=19)} #default args used by DAGs
+
 Base = sq.orm.declarative_base()
 
-class Bitcoin_table(Base):
+
+class BitcoinTable(Base):
+    '''Class to be used to create an initialization schema of the destination table in SQLite. ORM approach used'''
+
     __tablename__ = "bitcoin_price"
 
     id = sq.Column(sq.Text, primary_key=True)
@@ -20,18 +25,17 @@ class Bitcoin_table(Base):
     rate_USD = sq.Column(sq.Float)
     rate_GBP = sq.Column(sq.Float)
 
-default_args = {
-    'start_date': datetime(year=2022, month=10, day=19)
-}
 
-def get_data(url: str, ti) -> None:
-
+def get_data(url: str, push_key: str, ti) -> None:
+    '''Function that allows to get data from the URL & push it'''
     res = requests.get(url)
     json_list = json.loads(res.content)
-    ti.xcom_push(key='extracted_bitcoin_data', value=json_list)
+    ti.xcom_push(key=push_key, value=json_list)
 
-def transform_data(ti) -> None:
-    data = ti.xcom_pull(key='extracted_bitcoin_data', task_ids=['extract_bitcoin_data'])[0]
+
+def transform_data(pull_key: str, push_key: str, ti) -> None:
+    '''Function that transforms the pulled data, specified for the bitcoin API structure'''
+    data = ti.xcom_pull(key=pull_key, task_ids=['extract_bitcoin_data'])[0]
 
     id = str(uuid.uuid4())
     date = data.get('time', None).get('updated', None)
@@ -41,23 +45,29 @@ def transform_data(ti) -> None:
 
     row = {'id': id, 'date': date, 'rate_EUR': rate_EUR, 'rate_USD': rate_USD, 'rate_GBP': rate_GBP}
 
-    ti.xcom_push(key='transformed_bitcoin_data', value=row)
+    ti.xcom_push(key=push_key, value=row)
 
-def load_data(ti) -> None:
-    data = ti.xcom_pull(key='transformed_bitcoin_data', task_ids=['transform_bitcoin_data'])[0]
+
+def load_data(pull_key: str, ti) -> None:
+    '''Function that loads the data into the DB'''
+
+    data = ti.xcom_pull(key=pull_key, task_ids=['transform_bitcoin_data'])[0]
     engine = sq.create_engine("sqlite:///bitcoin1.db", echo=True, future=True)
     Base.metadata.create_all(engine, checkfirst = True)
     with sq.orm.Session(engine) as session:
-        row_to_insert = Bitcoin_table(**data)
+        row_to_insert = BitcoinTable(**data)
         session.add_all([row_to_insert])
         session.commit()
 
+
 def print_content(ti) -> None:
+    '''Function that prints pushes all the records from the DB - just to supervise'''
     engine2 = sq.create_engine("sqlite:///bitcoin1.db", echo=True, future=True)
     with engine2.connect() as e:
-        d = e.exec_driver_sql('SELECT * FROM bitcoin_price')
-        for i in d:
+        data = e.exec_driver_sql('SELECT * FROM bitcoin_price')
+        for i in data:
             ti.xcom_push(key='selected_data'+str(list(i)[0]), value=list(i))
+
 
 with DAG(
     dag_id='bitcoin_data_collector',
@@ -70,17 +80,19 @@ with DAG(
     task_extract_data = PythonOperator(
         task_id='extract_bitcoin_data',
         python_callable=get_data,
-        op_kwargs={'url': 'https://api.coindesk.com/v1/bpi/currentprice.json'}
+        op_kwargs={'url': 'https://api.coindesk.com/v1/bpi/currentprice.json', 'push_key': 'extracted_data'}
     )
     # Task 2 - Transform bitcoin data
     task_transform_data = PythonOperator(
         task_id='transform_bitcoin_data',
-        python_callable=transform_data
+        python_callable=transform_data,
+        op_kwargs={'pull_key': 'extracted_data', 'push_key': 'transformed_data'}
     )
     # Task 3 - Load bitcoin data into DB
     task_load_data = PythonOperator(
         task_id='load_data',
-        python_callable=load_data
+        python_callable=load_data,
+        op_kwargs={'pull_key': 'transformed_data'}
     )
     # Task 4 - Select data from DB for as a check
     task_print_content = PythonOperator(
